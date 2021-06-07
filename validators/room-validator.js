443630813af0f7c50842ebdd3b599fbc52071298
rufaidulk <rufaidulk999@gmail.com>
2021-06-07 12:10:02 +0530

    validation fix

--- validators/room-validator.js
+++ validators/room-validator.js
@@ -1,6 +1,21 @@
 const { checkSchema } = require('express-validator');
+const Room = require('../models/room-model');
 
 module.exports = checkSchema({
+    id: {
+        custom: {
+            options: (value) => {
+                return Room.findOne({ _id: value }).then(room => {
+                    if (Object.is(room, null)) {
+                        return Promise.reject('Room does not exist');
+                    }
+                    if (room.type == 'private') {
+                        return Promise.reject('Room is not a group');
+                    }
+                })
+            }
+        }
+    },
     name: {
         notEmpty: {
             errorMessage: 'Name is required',
